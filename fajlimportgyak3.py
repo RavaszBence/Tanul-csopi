c70c20a40ea4505a3b271cc41cdf191938023008
RavaszBence <bence254.sly@gmail.com>
2026-03-23 19:33:49 +0100

    python gyak dogara

--- fajlimportgyak3.py
+++ fajlimportgyak3.py
@@ -1,8 +1,13 @@
+szavak2 = []
 with open("alma.txt", "r", encoding="UTF8") as fájl:
     szöveg = fájl.read()
 
+
 print(f"A szöveg karakter száma: {len(szöveg)}")
 
+# butus_jelek = [".", ",", ";", "?", "!", "[", "]"]
+# for jel in butus_jelek:
+#     szöveg = szöveg.replace(jel, " ")
 sortor = []
 for sor in szöveg.split("\n"): # sorok megszámolása lista bejárásával
     sortor.append(sor)
@@ -12,11 +17,62 @@ szöveg = szöveg.lower()
 
 szavak = szöveg.split()
 
+otleghosszabb = []
+
+for x in range(5):
+    leghosszabb = ""
+    
+    for szo in szavak:
+        if len(szo) >= len(leghosszabb):
+            leghosszabb = szo
+            
+    otleghosszabb.append(leghosszabb)
+    szavak.remove(leghosszabb)
+    
+print(otleghosszabb)
+
+szavak2 = szöveg.split()
+
 alma_szo_hanyszor = []
-for szó in szavak:
+x = -1
+alma_szo_indexek = []
+for szó in szavak2:
+    x += 1
     if szó == "alma":
+        print(szó)
+        alma_szo_indexek.append(x)
         alma_szo_hanyszor.append(szó)
         
 print(alma_szo_hanyszor)
 print(len(alma_szo_hanyszor))
+print(alma_szo_indexek)
+
+mondatok = szöveg.split(".")
+
+
+harom_leghossz = []
+for x in range(3):
+    leghosszmond = ""
+    
+    for mondat in mondatok:
+        if len(mondat) >= len(leghosszmond):
+            leghosszmond = mondat
+    harom_leghossz.append(leghosszmond)
+    mondatok.remove(leghosszmond)
+    
+print(harom_leghossz)
+
+mondatok2 = szöveg.split(".")
+
+harom_legrovidebb = []
+for x in range(3):
+    legrovmondd = leghosszmond
+    
+    for mondat in mondatok2:
+        if len(mondat) <= len(legrovmondd):
+            legrovmondd = mondat
+    harom_legrovidebb.append(legrovmondd)
+    mondatok2.remove(legrovmondd)
+    
+print("\n", harom_legrovidebb)
 
